66db5f9ef1101e6ec33e6d4caaf77741e68cc0c4
Masaki Kono <masaki19931015@gmail.com>
2022-07-28 23:40:16 +0900

    add: components & screens dir

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
-import 'constants.dart';
+import '../components/icon_content.dart';
+import '../components/reusable_card.dart';
+import '../constants.dart';
 
 enum Gender {
   male,
